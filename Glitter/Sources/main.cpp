779f4abebebe0ba2e935d374a1ceed593acefb71
roselite-iris <jerry33mxy@sina.com>
2022-12-11 21:55:33 +0800

    final

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -139,8 +139,8 @@ GLFWwindow* window;
 QuadScreen quad_screen;
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1600;
+const unsigned int SCR_HEIGHT = 1200;
 
 // camera
 Camera camera(glm::vec3(0.0f, 2.5f, 2.0f));
